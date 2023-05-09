a5721e4bfdd633cf8ec1adf4aee260244277f550
PinguHackerino <109966417+PinguHackerino@users.noreply.github.com>
2023-05-09 17:51:51 +0200

    Update 0.4
    New implemantion of semaphore in Java: Version of the code 0.4

--- package consumatore;.java
+++ package consumatore;.java
@@ -1,30 +1,76 @@
-package produttore_consumatore;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Semaphore;
 
-public class consumatore extends Thread{
+public class Produttore_Consumatore {
+    public static void main(String[] args) {
 
-private Semaphore semaforo;
-private List<Integer> canale;
-Public Consumatore(Semaphore semaforo, List <Integer> canale){
-    this.semaforo = semaforo;
-    this.canale = canale;
+        Semaphore semaforo = new Semaphore(0);
+        List<Integer> canale = new ArrayList<Integer>();
+
+        Consumatore consumato = new Consumatore(semaforo, canale);
+        consumato.start();
+
+        Produttore produttore = new Produttore(semaforo, canale);
+        produttore.start();
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        produttore.interrupt();
+        consumato.interrupt();
+    }
 }
 
-@Override
-public void run(){
-    while(!isInterrupted()) {
-        try{
-            semaforo.acquire();
-        }
-        catch(InterruptedException e){
-            return;
-        }
-        synchronized(canale){
-            System.out.println("Ricevuto: " + canale);
+class Consumatore extends Thread {
+
+    private Semaphore semaforo;
+    private List<Integer> canale;
+
+    public Consumatore(Semaphore semaforo, List<Integer> canale) {
+        this.semaforo = semaforo;
+        this.canale = canale;
+    }
+
+    @Override
+    public void run() {
+        while (!isInterrupted()) {
+            try {
+                semaforo.acquire();
+            } catch (InterruptedException e) {
+                return;
+            }
+            synchronized (canale) {
+                Integer dato = canale.remove(0);
+                System.out.println("Ricevuto: " + dato);
+            }
         }
     }
 }
 
+class Produttore extends Thread {
+    private Semaphore semaforo;
+    private List<Integer> canale;
+
+    public Produttore(Semaphore semaforo, List<Integer> canale) {
+        this.semaforo = semaforo;
+        this.canale = canale;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        while (!isInterrupted()) {
+            synchronized (canale) {
+                Integer dato = random.nextInt(99);
+                System.out.println("Ho prodotto il dato: " + dato);
+                canale.add(dato);
+            }
+            semaforo.release();
+        }
+    }
 }
